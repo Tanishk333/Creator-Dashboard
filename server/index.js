8a67bb2b563feb53fc78f9f3343b8d60ffdff38f
Tanishk333 <tanishkagrawal333@gmail.com>
2025-05-04 01:46:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://creatordashboar.netlify.app' }));
 app.use(express.json());
 
 // Connect to MongoDB
